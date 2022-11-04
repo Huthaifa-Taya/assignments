2968bc3d5ea8e5486aadbdc5b9473b8c67acbb2b
taya huthaifa <thuthaifa@everconnect.com>
2022-11-04 10:27:41 +0300

    changed the repo structure to include all related files for the first assignment in one dir and created subclasses for car/van/motor that inheritecs Vehicle class

---
+++ rent_assignment/Vehicles.rb
@@ -0,0 +1,22 @@
+require "./Vehicle"
+
+class Car < Vehicle
+  def initialize
+    super
+    @rent_price = 50
+  end
+end
+
+class Van < Vehicle
+  def initialize
+    super
+    @rent_price = 70
+  end
+end
+
+class MotorCycle < Vehicle
+  def initialize
+    super
+    @rent_price = 20
+  end
+end
